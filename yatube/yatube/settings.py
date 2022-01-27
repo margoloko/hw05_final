c98a1902676377efa7fd9f0f75b6d8234896d490
Marina <margoloko@mail.ru>
2022-01-27 21:49:15 +0700

    pythonanywhere

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -29,7 +29,9 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '[::1]',
-    'testserver', ]
+    'testserver',
+    'www.margoloko.pythonanywhere.com',
+    'margoloko.pythonanywhere.com', ]
 
 
 # Application definition
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -56,8 +59,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 ROOT_URLCONF = 'yatube.urls'
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -30,5 +30,8 @@ handler500 = 'core.views.server_error'
 handler403 = 'core.views.permission_denied'
 
 if settings.DEBUG:
+
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
