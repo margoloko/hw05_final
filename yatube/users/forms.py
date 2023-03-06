b0275f31f75187f8eb2ce1580dcfd7cca768bde3
Balakhonova Marina <90852680+margoloko@users.noreply.github.com>
2023-03-06 17:24:09 +0300

    Update forms.py

--- yatube/users/forms.py
+++ yatube/users/forms.py
@@ -6,7 +6,12 @@ User = get_user_model()
 
 
 class CreationForm(UserCreationForm):
-
+    """The CreationForm class is then defined as a subclass of UserCreationForm.
+    It overrides the Meta class, which is a class used by Django to define metadata for a form or model.
+    In this case, it sets the model attribute of the Meta class to User, which is the user model class
+    returned by get_user_model().
+    It also sets the fields attribute to a tuple of field names that should be included in the form,
+    including first_name, last_name, username, and email."""
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('first_name', 'last_name', 'username', 'email')
