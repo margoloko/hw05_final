4e819fa38ad36674804611730813f7a3b7f83f9f
Balakhonova Marina <90852680+margoloko@users.noreply.github.com>
2023-03-06 17:30:02 +0300

    Update views.py

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -5,6 +5,12 @@ from .forms import CreationForm
 
 
 class SignUp(CreateView):
+    """The CreationForm class is then defined as a subclass of UserCreationForm.
+    It overrides the Meta class, which is a class used by Django to define metadata for a form or model.
+    In this case, it sets the model attribute of the Meta class to User,
+    which is the user model class returned by get_user_model().
+    It also sets the fields attribute to a tuple of field names that should be
+    included in the form, including first_name, last_name, username, and email."""
 
     form_class = CreationForm
     success_url = reverse_lazy('posts:index')
