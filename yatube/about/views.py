c864076fa6cbce1659a93c575895800e8c5e0123
Balakhonova Marina <90852680+margoloko@users.noreply.github.com>
2023-03-09 05:33:10 +0300

    Update views.py
    add documentations

--- yatube/about/views.py
+++ yatube/about/views.py
@@ -2,8 +2,17 @@ from django.views.generic.base import TemplateView
 
 
 class AboutAuthorView(TemplateView):
+    """
+    The AboutAuthorView renders the 'about/author.html' template,
+    which contains information about the author of the website.
+    """
     template_name = 'about/author.html'
 
 
 class AboutTechView(TemplateView):
+    """
+    The AboutTechView renders the 'about/tech.html' template,
+    which contains information about the technology
+    used to build the website.
+    """
     template_name = 'about/tech.html'
